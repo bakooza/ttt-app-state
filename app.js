dd871ed87cd8f7b6bbebd26d9f8e0f927e0a1b91
Burkey Koontz <bk.koontz@gmail.com>
2018-07-23 13:09:27 -0700

    added boilerplate modules

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const state = {
+const state = { 
+  board: [ [0,0,0], 
+           [0,0,0], 
+           [0,0,0] ]
+              }
 };
 
 // State modification functions
@@ -6,3 +10,9 @@ const state = {
 // Render functions
 
 // Event Listeners
+
+// Upon loading, I can immediately click cells to alternately place Xs and Os on each click
+// I cannot change a cell after it has a value inside it
+// I can see when a winning line has been created
+// No further moves can be played from this point
+// I can click "New Game" at any time to reset the board
